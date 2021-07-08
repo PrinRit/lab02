ed8e1c6a7d9b5e00acf395e827c285a38da160ce
Prin Ritti-traiphop <prinr2481@gmail.com>
2021-07-08 19:54:57 +0700

    3.4 remove image and inStock out and then see (complt V.)

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const app = Vue.createApp({
         return {
             product: 'Shoes',
             brand: 'SE 331',
-            image: './assets/images/socks_green.jpg',
-            inStock: true,
+            //image: './assets/images/socks_green.jpg',
+            //inStock: true,
             inventory: 100,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [

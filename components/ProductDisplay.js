b43f3b9fdba1024d2f689a6b9c810dc8749468cf
Prin Ritti-traiphop <prinr2481@gmail.com>
2021-07-09 00:09:01 +0700

    6.3-6.8 add review list, hide review list, alert the review when you blank some infomation and add the reviews to window

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -28,7 +28,8 @@ app.component('product-display',{
                     <button class=" button " :disabled='!inStock' :class="{disabledButton: !inStock}" @click="removeCart">Remove Cart</button>
                 </div>
             </div>
-            <review-form></review-form>
+            <review-list v-if="reviews.length" :reviews="reviews"></review-list>
+            <review-form @review-submitted="addReview"></review-form>
         </div>`
     ,data() {
         return {
@@ -41,6 +42,7 @@ app.component('product-display',{
                 { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
             ],
             selectedVariant: 0,
+            reviews:[]
         }
     },
     methods: {
@@ -56,6 +58,9 @@ app.component('product-display',{
         },
         updateVariant(index) {
             this.selectedVariant = index
+        },
+        addReview(review){
+            this.reviews.push(review)
         }
     },
     computed: {
--- components/ReviewForm.js
+++ components/ReviewForm.js
@@ -1,16 +1,16 @@
 app.component('review-form',{
     template:
     /*html*/
-    `<form class="review-form">
+    `<form class="review-form" @submit.prevent="onSubmit">
         <h3>Leave a review</h3>
         <label for="name">Name:</label>
-        <input id="name">
+        <input id="name" v-model="name">
         
         <label for="review">Review</label>
-        <textarea id="review"></textarea>
+        <textarea id="review" v-model="review"></textarea>
         
         <label for="rating">Rating</label>
-        <select id="rating">
+        <select id="rating" v-model.number="rating">
             <option>5</option>
             <option>4</option>
             <option>3</option>
@@ -25,6 +25,23 @@ app.component('review-form',{
             name:'',
             review:'',
             rating: null
+        }
+    },
+    methods: {
+        onSubmit(){
+            if (this.name === '' || this.review === '' || this.rating === null) {
+                alert('Review is incomplete. Please fill out every field.')
+                return
+            }
+            let productReview = {
+                name: this.name,
+                review: this.review,
+                rating: this.rating
+            }
+            this.$emit('review-submitted',productReview)
+            this.name = ''
+            this.review = ''
+            this.rating = null
         }
     }
 })
